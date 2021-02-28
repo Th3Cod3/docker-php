802425285f9c8def8e947a4701115b1a14cdd760
Th3Cod3 <yefri@th3cod3.com>
2021-02-28 19:12:53 +0100

    update to php 8 and implement some script for self signed cer

---
+++ .docker/config.sh
@@ -0,0 +1,53 @@
+#!/usr/bin bash
+if [[ $PHALCON == 1 ]]; then
+  pecl install phalcon
+  docker-php-ext-enable phalcon
+  composer global require phalcon/devtools
+fi
+
+if [[ $SSL_SELF_SIGNED == 1 ]]; then
+  echo "######## executing Self-Signed Certificate $DOMAIN ########"
+  ## Edit files
+  cat v3.ext | sed s/%%DOMAIN%%/"$DOMAIN"/g > _v3.ext
+  cat server.conf | sed s/%%DOMAIN%%/"$DOMAIN"/g > _server.conf
+  rm server.conf
+  cat _server.conf | sed s/%%DOCUMENT_ROOT%%/"$DOCUMENT_ROOT"/g > server.conf
+
+  ## Create ROOT certificate
+  openssl genrsa -out rootCA.key 2048
+  openssl req -x509 -new -nodes -key rootCA.key \
+    -sha256 -days 1024 -out rootCA.pem -subj $SUBJ_CERT
+
+  ## Create DOMAIN certificate
+  openssl req -new -newkey rsa:2048 -sha256 -nodes \
+    -keyout server.key \
+    -out server.csr -subj $SUBJ_CERT
+  openssl x509 -req -in server.csr -CA rootCA.pem \
+    -CAkey rootCA.key -CAcreateserial -out server.crt \
+    -days 365 -sha256 -extfile _v3.ext
+
+  # Copy files
+  cp server.key /etc/ssl/private/server.key
+  cp server.crt /etc/ssl/certs/server.crt
+  cp server.conf /etc/apache2/sites-available/$DOMAIN.conf
+
+  # Setting up apache
+  a2enmod ssl
+  a2ensite $DOMAIN.conf
+  a2dissite 000-default.conf
+  a2dissite default-ssl.conf
+  echo "######## Setup apache VirtualHost $DOMAIN #########"
+else
+  echo "######## Without SSL ########"
+  echo -e "\nServerName $DOMAIN" >> /etc/apache2/sites-available/000-default.conf
+  cat /etc/apache2/sites-available/000-default.conf |
+    sed s/localhost/"$DOMAIN\n\tServerName $DOMAIN"/g > /etc/apache2/sites-available/_000-default.conf
+  cp /etc/apache2/sites-available/_000-default.conf /etc/apache2/sites-available/000-default.conf
+fi
+
+# Setting up apache
+echo -e "127.0.0.1\t$DOMAIN" >> /etc/hosts
+echo "ServerName $DOMAIN" >> /etc/apache2/apache2.conf
+a2enmod rewrite
+service apache2 restart
+apache2ctl configtest
--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # Docker
+If you want to use the containers you can just run the following instruction:
+
+* Edit container args into `docker-compose.yml`
+  * `DOMAIN=<domain>` For the domain request
+  * `SSL_SELF_SIGNED=<0|1>` 1 to set up self-signed certificate or 0 to set without ssl
+  * `PHALCON=<0|1>` 1 to enable phalcon
+  * `DOCUMENT_ROOT=<0|1>` Here come de entry point of Apache
 * Run docker-compose
-  * `docker-compose up -d`
+  * `docker-compose up -d` if it's the first time
+  * `docker-compose up -d --build` to build the container again
+
+## if you use the self-signed certificate
 * Add the host, override DNS.
   * Windows: `C:\Windows\System32\drivers\etc\host` open as Administrator.
   * Mac OS/Linux: `/etc/hosts`.
 * Get the rootCA.pem from the container `/var/server-conf/rootCA.pem`.
-  * `docker-compose exec rsi bash`
-  * `cat /var/server-conf/rootCA.pem`
+  * `docker-compose exec dtt cat /var/server-conf/rootCA.pem`
 * Save the key and import to the Thrusted Root Certification Authorities
